d3a9c75104b943bc21641c56b9c21a1c83723363
ojam1 <oranson1@gmail.com>
2017-06-11 12:17:59 +0100

    Added Test Class

--- FizzBuzz.java
+++ FizzBuzz.java
@@ -1,9 +1,7 @@
 package com.ojam1;
 
-class FizzBuzz
-{
-    static void fizzBuzzer(int x)
-    {
+class FizzBuzz {
+    static void fizzBuzzer(int x) {
         if (fizzBuzzChecker(x)) {
             if (x % 3 == 0 && x % 5 == 0) {
                 System.out.println("FizzBuzz");
@@ -14,47 +12,39 @@ class FizzBuzz
         else System.out.println(x);
     }
 
-    private static boolean fizzBuzzChecker2 (int x)
-    {
+    private static boolean fizzBuzzChecker2(int x) {
         boolean found = false;
         int count = 0;
         String s = "";
         String intAsString = Integer.toString(x);
-        for (int i=0; i<intAsString.length();i++)
-        {
-
-            if (intAsString.charAt(i)=='3')
-            {
+        for (int i = 0; i < intAsString.length(); i++) {
+            if (intAsString.charAt(i) == '3') {
                 s = "Fizz";
                 found = true;
                 count++;
-            } else if (intAsString.charAt(i)=='5')
-            {
+            } else if (intAsString.charAt(i) == '5') {
                 s = "Buzz";
                 found = true;
                 count++;
             }
-
-        } if (count==2)
-    {
-        s = "FizzBuzz";
-    }
-        System.out.println(s);
+        }
+        if (count > 0) {
+            if (count > 1) {
+                s = "FizzBuzz";
+                System.out.println(s);
+            } else System.out.println(s);
+        }
         return found;
     }
 
-    private static boolean fizzBuzzChecker(int x)
-    {
+    private static boolean fizzBuzzChecker(int x) {
         return (x % 3 == 0 || x % 5 == 0);
     }
 }
 
-class Main
-{
-    public static void main(String[] args)
-    {
-        for (int i = 1;i<=100;i++)
-        {
+class Main {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 1000; i++) {
             FizzBuzz.fizzBuzzer(i);
         }
     }
---
+++ FizzBuzzTest.java
@@ -0,0 +1,13 @@
+package com.ojam1;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * Created by Oliver on 11/06/2017.
+ */
+class FizzBuzzTest {
+    @org.junit.jupiter.api.Test
+    void fizzBuzzer() {
+    }
+
+}
