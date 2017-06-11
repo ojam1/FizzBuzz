0837798cc01a2110c184a6c9fb9b7a436494bddb
ojam1 <oranson1@gmail.com>
2017-06-11 16:09:41 +0100

    Update

--- FizzBuzzTest.java
+++ FizzBuzzTest.java
@@ -6,10 +6,6 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
  * Created by ojam1 on 11/06/2017.
  */
 class FizzBuzzTest {
-//    @org.junit.jupiter.api.Test
-//    void fizzBuzzer() {
-//        assertEquals("FizzBuzz", FizzBuzz.fizzBuzzer(53));
-//    }
 
     @org.junit.jupiter.api.Test
     void divisibleByThree() {
@@ -23,10 +19,39 @@ class FizzBuzzTest {
     void divisibleByFive() {
         int[] testValues = {5, 10, 20, 25, 35, 40, 50, 55};
         for (int i : testValues) {
-            assertEquals("Buzz",FizzBuzz.fizzBuzzer(i));
+            assertEquals("Buzz", FizzBuzz.fizzBuzzer(i));
         }
     }
 
     @org.junit.jupiter.api.Test
-    void containsThree()
+    void containsThree() {
+        int[] testValues = {13, 23, 43, 31, 32, 34, 37, 38, 73};
+        for (int i : testValues) {
+            assertEquals("Fizz", FizzBuzz.fizzBuzzer(i));
+        }
+    }
+
+    @org.junit.jupiter.api.Test
+    void containsFive() {
+        int[] testValues = {52, 56, 58, 59, 151, 152, 157};
+        for (int i : testValues) {
+            assertEquals("Buzz", FizzBuzz.fizzBuzzer(i));
+        }
+    }
+
+    @org.junit.jupiter.api.Test
+    void divisibleByThreeAndFive() {
+        int[] testValues = {15, 30, 45, 60};
+        for (int i : testValues) {
+            assertEquals("FizzBuzz", FizzBuzz.fizzBuzzer(i));
+        }
+    }
+
+    @org.junit.jupiter.api.Test
+    void containsThreeAndFive() {
+        int[] testValues = {53,253};
+        for (int i : testValues) {
+            assertEquals("FizzBuzz", FizzBuzz.fizzBuzzer(i));
+        }
+    }
 }
